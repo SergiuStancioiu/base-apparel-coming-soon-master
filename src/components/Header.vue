4e69f2da4f5f4eff133356c5fb2870b1d1de4d3d
Sergiu Stancioiu <s.sergiu96@gmail.com>
2025-11-26 22:58:09 +0200

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+import image from '../assets/vue.svg';
+</script>
+
+<template>
+  <header>
+    <img :src="image" alt="image" />
+    <h1>{{ msg }}</h1>
+  </header>
+</template>
